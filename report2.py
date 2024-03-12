0180e50a3a6b3072f5ea76f6b336384e5736fe17
Jjy0211 <162521939+Jjy0211@users.noreply.github.com>
2024-03-12 22:57:24 +0900

    Add files via upload

---
+++ report2.py
@@ -0,0 +1,20 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("첫 번째 숫자를 입력하세요 : "))
+첫 번째 숫자를 입력하세요 : 1
+>>> b=int(input("두 번째 숫자를 입력하세요 : "))
+두 번째 숫자를 입력하세요 : 5
+>>> result=a
+>>> result=a
+>>> result=a+b
+>>> print(a,"+",b,"=",result)
+1 + 5 = 6
+>>> result=a-b
+>>> print(a,"-",b,"=",result)
+1 - 5 = -4
+>>> result=a*b
+>>> print(a,"*",b,"=",result)
+1 * 5 = 5
+>>> result=a/b
+>>> print(a,"/",b,"=",result)
+1 / 5 = 0.2
